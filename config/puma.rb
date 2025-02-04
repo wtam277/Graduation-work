55408b315009530319772e692cd5b4cea24e12eb
滝沢彩翔 <takizawaayato@takizawaayashounonotobukkukonpyuta.local>
2025-02-04 18:21:10 +0900

    333333

--- config/puma.rb
+++ config/puma.rb
@@ -24,8 +24,13 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+#port ENV.fetch("PORT", 3000)
 
+ssl_bind '0.0.0.0', '3000', {
+  key: 'config/ssl/server.key',
+  cert: 'config/ssl/server.crt',
+  verify_mode: 'none'
+}
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
