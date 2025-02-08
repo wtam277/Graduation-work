2d58c39cdab745677dadd485f5a4c2493d0e711c
滝沢彩翔 <takizawaayato@takizawaayashounonotobukkukonpyuta.local>
2025-02-08 10:02:12 +0900

    009999999

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,12 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# port ENV.fetch("PORT", 3000)
+
+#dockerからlocalhostにアクセス用
+bind "tcp://0.0.0.0:3000"
+
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
