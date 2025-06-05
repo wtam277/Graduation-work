5f280413897dadb956dedd0b53f7912b2cb1295c
滝沢彩翔 <takizawaayato@takizawaayashounonotobukkukonpyuta.local>
2025-06-05 18:06:36 +0900

    02resetpassword

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'graduation-work.onrender.com', # ←アプリのドメイン名またはgmail.comでも可
+    domain:               'gmail.com', # ←アプリのドメイン名またはgmail.comでも可
     user_name:            ENV['MAILER_SENDER'],
     password:             ENV['MAILER_PASSWORD'],
     authentication:       'plain',
